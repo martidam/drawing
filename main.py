0523f5c65d5e3c3613d3bfc05813fed83a37f651
Damario Martin <martidam002@warren.k12.in.us>
2024-10-29 09:15:55 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age"))
+if age < 5:
+     print("You should play with toys.")
+elif age >= 5 and age <= 12:
+     print("You can play outdoor game.")
+elif age >=13 and age <=17:
+    print("You should play video games")
+else:
+    print("You should go hiking")
+
